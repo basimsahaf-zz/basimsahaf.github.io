448f6f6516558f98b56b0d430957738bf8797ad9
basimsahaf <bssahaf@uwaterloo.ca>
2017-05-07 01:59:41 -0400

    front-end touchups

--- EarthyResources/JS/script.js
+++ EarthyResources/JS/script.js
@@ -1,5 +1,13 @@
 function changeIcon (){
     if($("#searchButton".click)){
+        $("#logo").hide();
         $("#tagLine").hide();
+        $("#searchBar").addClass("animated fadeInUp");
+        $("#searchButton").addClass("animated fadeInUp");
+        $("#logoAfter").text("Earthy");
+        $("#logoAfter").addClass("animated zoomIn");
+        $("#tagLineAfter").text("A Bucketful Of Science");
+        $("#tagLineAfter").addClass("animated zoomIn");
     }
+    return false;
 }
