35c5a8e6496d212970e824c177cde07505527b8a
basimsahaf <bssahaf@uwaterloo.ca>
2017-05-31 04:55:08 -0400

    fixed the image bug

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.querySelector('.btn-roll').addEventListener('click', function (){
         var dice = Math.floor(Math.random()*6) + 1;
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
-        diceDOM.src = '/resources/dice-' + dice + '.png';
+        diceDOM.src = 'dice-' + dice + '.png';
     
         if(dice != 1) {
             roundScore += dice;
